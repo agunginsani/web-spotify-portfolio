5279d109286ad376668a1c427a1e433adf1b8188
agunginsani <agunginsanialam@gmail.com>
2024-01-27 09:45:35 +0700

    feat: use defer on album page

--- app/routes/search.$query.albums.tsx
+++ app/routes/search.$query.albums.tsx
@@ -1,5 +1,6 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import {
+  defer,
   isRouteErrorResponse,
   useFetcher,
   useLoaderData,
@@ -21,7 +22,7 @@ const schema = z.object({
       z.object({
         name: z.string(),
         id: z.string(),
-        release_date: z.coerce.date(),
+        release_date: z.string(),
         images: z.array(
           z.object({
             url: z.string().url(),
@@ -52,79 +53,33 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   searchParams.set("limit", limit.toString());
   searchParams.set("market", "ID");
 
-  const response = await client(
+  const response = client(
     request,
     `https://api.spotify.com/v1/search?${searchParams}`,
-  );
-
-  if (response.ok) {
-    const data = schema.parse(await response.json());
-    return data;
-  }
-
-  if (response.status === 400) {
-    return {
-      albums: {
+  ).then(async (response) => {
+    if (response.ok) {
+      const data = schema.parse(await response.json());
+      return data.albums;
+    }
+    if (response.status === 400) {
+      return {
         items: [],
         offset: 0,
         limit: 0,
         previous: null,
         next: null,
         total: 0,
-      },
-    } satisfies z.infer<typeof schema>;
-  }
+      } satisfies z.infer<typeof schema>["albums"];
+    }
+    throw response;
+  });
 
-  throw response;
+  return defer({ albums: response });
 }
 
 export default function Route() {
-  const data = useLoaderData<typeof loader>();
-  const albums = data.albums;
-  const params = useParams();
-
   const error = useRouteError();
-  const { load, ...fetcher } = useFetcher<z.infer<typeof schema>>();
-
-  const [infiniteItems, setInfiniteItems] = useState(albums.items);
-  const [offset, setOffset] = useState(albums.offset);
-  const observer = useRef<IntersectionObserver>();
-  const searchInputRef = useRef<HTMLInputElement>(null);
-  const lastItemRef = useCallback((node: HTMLLIElement) => {
-    observer.current?.disconnect();
-    observer.current = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        setOffset((prev) => prev + limit);
-      }
-    });
-
-    if (node instanceof Element) {
-      observer.current.observe(node);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (fetcher.data) {
-      const items = fetcher.data.albums.items;
-      setInfiniteItems((prevs) => [...prevs, ...items]);
-      if (fetcher.data.albums.next === null) observer.current?.disconnect();
-    }
-  }, [fetcher.data]);
-
-  useEffect(() => {
-    if (offset > albums.offset) {
-      const searchParams = new URLSearchParams();
-      searchParams.append("offset", offset.toString());
-      searchParams.append("type", params.type!);
-      searchParams.append("q", searchInputRef.current?.value ?? "");
-      load(`?${searchParams}`);
-    }
-  }, [albums.offset, load, offset, params.type]);
-
-  useEffect(() => {
-    setOffset(0);
-    setInfiniteItems(albums.items);
-  }, [albums.items]);
+  const { infiniteItems, fetcher, lastItemRef } = useInfiniteItems();
 
   if (isRouteErrorResponse(error)) {
     return <div>{error.data}</div>;
@@ -213,4 +168,57 @@ function AlbumCard({ avatar, name, year, artist }: AlbumCard) {
       </div>
     </button>
   );
+}
+
+type AlbumItems = z.infer<typeof schema>["albums"]["items"];
+
+function useInfiniteItems() {
+  const data = useLoaderData<typeof loader>();
+  const initialOffset = 0;
+  const params = useParams();
+  const { load, ...fetcher } = useFetcher<typeof loader>();
+  const [infiniteItems, setInfiniteItems] = useState<AlbumItems>([]);
+  const [offset, setOffset] = useState(initialOffset);
+  const observer = useRef<IntersectionObserver>();
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  const lastItemRef = useCallback((node: HTMLLIElement) => {
+    observer.current?.disconnect();
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        setOffset((prev) => prev + limit);
+      }
+    });
+
+    if (node instanceof Element) {
+      observer.current.observe(node);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (fetcher.data) {
+      Promise.resolve(fetcher.data.albums).then(({ items, next }) => {
+        setInfiniteItems((prevs) => [...prevs, ...items]);
+        if (next === null) observer.current?.disconnect();
+      });
+    }
+  }, [fetcher.data]);
+
+  useEffect(() => {
+    if (offset > initialOffset) {
+      const searchParams = new URLSearchParams();
+      searchParams.append("offset", offset.toString());
+      searchParams.append("type", params.type!);
+      searchParams.append("q", searchInputRef.current?.value ?? "");
+      load(`?${searchParams}`);
+    }
+  }, [initialOffset, load, offset, params.type]);
+
+  useEffect(() => {
+    data.albums.then((album) => {
+      setOffset(0);
+      setInfiniteItems(album.items);
+    });
+  }, [data]);
+
+  return { infiniteItems, fetcher, lastItemRef };
 }
