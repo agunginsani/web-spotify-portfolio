9cdb9c7bd966ad0259ca45aa9b8035fa7a077c17
agunginsani <agunginsanialam@gmail.com>
2024-01-29 19:35:22 +0700

    chore: remove sensitive code

--- app/cookies.server.ts
+++ app/cookies.server.ts
@@ -1,8 +1,11 @@
 import { createCookie } from "@remix-run/node"; // or cloudflare/deno
+import { z } from "zod";
+
+const secret = z.string().parse(process.env.COOKIE_CREDENTIALS_SECRET);
 
 export const credentials = createCookie("credentials", {
   maxAge: 604_800,
-  secrets: ["WnN7J6jNdY"],
+  secrets: [secret],
   httpOnly: true,
   secure: true,
 });
