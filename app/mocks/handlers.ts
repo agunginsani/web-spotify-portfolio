4a460eaacb6bcc413fcc81b1fa25e63e2be7b53d
Agung Insani <160093700+agunginsani99@users.noreply.github.com>
2024-07-21 22:03:57 +0700

    chore: setup `playwright`

--- app/mocks/handlers.ts
+++ app/mocks/handlers.ts
@@ -1,1 +1,27 @@
-export const handlers = [];
+import { http, HttpResponse } from "msw";
+
+export const handlers = [
+  http.get("https://api.spotify.com/v1/artists/1", () => {
+    return HttpResponse.json({
+      id: "1",
+      name: "Test",
+      followers: {
+        href: null,
+        total: 8,
+      },
+      images: [
+        {
+          url: "https://picsum.photos/300",
+          height: 300,
+          width: 300,
+        },
+      ],
+    });
+  }),
+  http.get(
+    "https://api.spotify.com/v1/me/following/contains?type=artist&ids=1",
+    () => {
+      return HttpResponse.json([false]);
+    },
+  ),
+];
