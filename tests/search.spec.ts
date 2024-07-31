8b6cc6ae9babc5ddb4fea7cd4695512c44b3f5e5
Agung Insani <160093700+agunginsani99@users.noreply.github.com>
2024-07-31 10:23:59 +0700

    test: update search tests
    add search albums flow

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
-test("can search artist", async ({ page }) => {
+test("can search artists", async ({ page }) => {
   await page.goto("/login?code=TEST");
 
   await page.goto("/");
@@ -9,19 +9,93 @@ test("can search artist", async ({ page }) => {
     name: "What do you want to listen to?",
   });
 
-  const list = page.getByRole("list", { name: "Artists" });
+  const artistsList = page.getByRole("list", { name: "Artists" });
+
+  const albumsList = page.getByRole("list", { name: "Albums" });
 
   await expect(searchbox).toBeVisible();
 
-  await expect(list).not.toBeVisible();
+  await expect(artistsList).not.toBeVisible();
+
+  await expect(albumsList).not.toBeVisible();
+
+  const artistsLink = page.getByRole("link", { name: "Artists" });
+
+  const albumsLink = page.getByRole("link", { name: "Albums" });
+
+  await expect(artistsLink).not.toBeVisible();
+
+  await expect(albumsLink).not.toBeVisible();
 
   await searchbox.fill("a");
 
-  await expect(page.getByRole("link", { name: "Artists" })).toBeVisible();
+  await expect(artistsLink).toBeVisible();
 
-  await expect(page.getByRole("link", { name: "Albums" })).toBeVisible();
+  await expect(artistsLink).toHaveAttribute("aria-current", "page");
 
-  await expect(list).toBeVisible();
+  await expect(albumsLink).toBeVisible();
 
-  expect(await list.getByRole("listitem").all()).toHaveLength(50);
+  await expect(albumsLink).not.toHaveAttribute("aria-current", "page");
+
+  await expect(artistsList).toBeVisible();
+
+  await expect(albumsList).not.toBeVisible();
+
+  expect(await artistsList.getByRole("listitem").all()).toHaveLength(50);
+});
+
+test("can search albums", async ({ page }) => {
+  await page.goto("/login?code=TEST");
+
+  await page.goto("/");
+
+  const searchbox = page.getByRole("searchbox", {
+    name: "What do you want to listen to?",
+  });
+
+  const artistsList = page.getByRole("list", { name: "Artists" });
+
+  const albumsList = page.getByRole("list", { name: "Albums" });
+
+  await expect(searchbox).toBeVisible();
+
+  await expect(artistsList).not.toBeVisible();
+
+  await expect(albumsList).not.toBeVisible();
+
+  const artistsLink = page.getByRole("link", { name: "Artists" });
+
+  const albumsLink = page.getByRole("link", { name: "Albums" });
+
+  await expect(artistsLink).not.toBeVisible();
+
+  await expect(albumsLink).not.toBeVisible();
+
+  await searchbox.fill("a");
+
+  await expect(artistsLink).toBeVisible();
+
+  await expect(artistsLink).toHaveAttribute("aria-current", "page");
+
+  await expect(albumsLink).toBeVisible();
+
+  await expect(albumsLink).not.toHaveAttribute("aria-current", "page");
+
+  await expect(artistsList).toBeVisible();
+
+  await expect(albumsList).not.toBeVisible();
+
+  expect(await artistsList.getByRole("listitem").all()).toHaveLength(50);
+
+  await albumsLink.click();
+
+  await expect(albumsLink).not.toHaveAttribute("aria-current", "page");
+
+  await expect(albumsLink).toHaveAttribute("aria-current", "page");
+
+  await expect(artistsList).not.toBeVisible();
+
+  await expect(albumsList).toBeVisible();
+
+  expect(await albumsList.getByRole("listitem").all()).toHaveLength(50);
 });
